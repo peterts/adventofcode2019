5bda40b25702f97b1a5aea5255a54799a895986d
= <petthosan@gmail.com>
2019-12-17 21:51:42 +0100

    Finished day 17, really ugly solution

--- src/rescue.py
+++ src/rescue.py
@@ -1,6 +1,8 @@
 from src.helpers import read_comma_separated_list, clean_lines_iter
 from src.int_program import IntProgram
 from itertools import chain
+from collections import Counter
+import re
 
 
 def is_intersection(lines, i, j, n, m):
@@ -32,8 +34,17 @@ def can_move(lines, i, j, direction):
     return -1 < k < n and -1 < l < m and lines[k][l] == "#"
 
 
-def expand_operations(operations):
-    return list(chain.from_iterable(x * count for x, count in zip(operations[::2], operations[1::2])))
+# def split_operations(operations_str, seq):
+#     prev_match = None
+#     splits = []
+#     for match in re.finditer(f"(?=({seq}))", operations_str):
+#         if prev_match is not None:
+#         pass
+
+
+def to_command(func):
+    print(len(','.join(re.findall(r"(?:[A-Z]|\d+)", func))))
+    return list(map(ord, ','.join(re.findall(r"(?:[A-Z]|\d+)", func)))) + [10]
 
 
 if __name__ == '__main__':
@@ -47,7 +58,6 @@ if __name__ == '__main__':
     m = None
 
     intersections = []
-    # graph = nx.Graph()
 
     robot_or_scaffold = "#>^v<"
 
@@ -62,14 +72,6 @@ if __name__ == '__main__':
                 n_scaffolds += 1
                 if robot_or_scaffold.index(lines[i][j]) > 0:
                     robot = (i, j)
-                # for k, l in (i-1, j), (i+1, j), (i, j-1), (i, j+1):
-                #     if k < 0 or l < 0:
-                #         continue
-                #     try:
-                #         if lines[k][l] in robot_or_scaffold:
-                #             graph.add_edge((i, j), (k, l))
-                #     except IndexError:
-                #         continue
 
             if is_intersection(lines, i, j, n, m):
                 intersections.append(i*j)
@@ -89,7 +91,7 @@ if __name__ == '__main__':
             steps += 1
         else:
             if steps > 0:
-                operations.append(steps)
+                operations.append(steps+1)
             steps = 0
 
             if can_move(lines, i, j, TURN_RIGHT[direction]):
@@ -107,27 +109,38 @@ if __name__ == '__main__':
         visited.add((i, j))
 
     if steps > 0:
-        operations.append(steps)
+        operations.append(steps+1)
     steps = 0
 
-    print(operations)
-    print(set([''.join(map(str, x)) for x in zip(operations[::2], operations[1::2])]))
-    print(expand_operations(operations))
+    operations_str = ' '.join(''.join(map(str, x)) for x in zip(operations[::2], operations[1::2]))
 
-    # graph = graph.to_undirected()
-    #
-    # paths = dict(nx.all_pairs_shortest_path(graph))
-    #
-    # graph2 = nx.Graph()
-    # edges = []
-    #
-    # for node in graph.nodes:
-    #     for other_node in graph.nodes:
-    #         if other_node == node:
-    #             continue
-    #         edges.append([node, other_node, len(paths[node][other_node])])
-    #
-    # graph2.add_weighted_edges_from(edges)
-    # print(dnx.traveling_salesperson(graph2, dimod.ExactSolver(), start=robot))
+    memory[0] = 2
+    program = IntProgram(memory)
 
+    A = "L6 R12 L6 L8 L8"
+    B = "L4 L4 L6"
+    C = "L6 R12 R8 L8"
+
+    print(operations_str)
+    operations_str = operations_str.replace(A, "A")
+    print(operations_str)
+    operations_str = operations_str.replace(B, "B")
+    print(operations_str)
+    operations_str = operations_str.replace(C, "C")
+    print(operations_str)
+
+    program.memory[0] = 2
+    print(program.state)
+
+    program.run(to_command(operations_str))
+    print(program.state)
+    program.run(to_command(A))
+    print(program.state)
+    program.run(to_command(B))
+    print(program.state)
+    program.run(to_command(C))
+    print(program.state)
+    program.run([ord('n'), 10])
+    print(program.state)
+    print(program.output)
 
