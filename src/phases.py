3e07bf43172d0f0a484000a773ca2a0b38cf1244
= <petthosan@gmail.com>
2019-12-17 08:08:05 +0100

    Start day 17

--- src/phases.py
+++ src/phases.py
@@ -1,75 +1,69 @@
 import math
+from functools import lru_cache
+from itertools import accumulate
 
 
-def compute_phase(values, base_pattern_matrix):
+def compute_phase(values, base_pattern, n_repeats=1):
     values_out = []
+    num_values_in_one_repetition = len(values) // n_repeats
+
     for i in range(len(values)):
-        values_out.append(
-            int(str(sum(x*p for x, p in zip(values, base_pattern_matrix[i])))[-1])
-        )
+        print(i)
+        cycle_time = get_cycle_time(base_pattern, num_values_in_one_repetition, i)
+        gen = yield_pattern(base_pattern, i+1)
+
+        cumsums = [0]
+        s = 0
+        for j in range(min(n_repeats+1, cycle_time+1)):
+            s += sum(x*p for x, p in zip(values, next_n(gen, num_values_in_one_repetition)))
+            cumsums.append(s)
+
+        x = n_repeats // (cycle_time + 1)
+        value = x * cumsums[-1]
+        value += cumsums[n_repeats - (x * (cycle_time + 1))]
+
+        values_out.append(last_digit(value))
+
     return values_out
 
 
-def create_base_pattern_matrix(base_pattern, n_digits):
-    m = []
-    for i in range(n_digits):
-        gen = yield_pattern(base_pattern, i+1)
-        m.append(list(next(gen) for _ in range(n_digits)))
-    return m
-
-
-def yield_pattern(base_pattern, n):
+def yield_pattern(base_pattern, n_repeats):
     skip = True
     while 1:
         for i in base_pattern:
-            for _ in range(n):
+            for _ in range(n_repeats):
                 if skip:
                     skip = False
                     continue
                 yield i
 
 
-def get_cycle_time(base_pattern, n):
-    cycle_time = []
-
-    for j in range(1, n+1):
-        gen = yield_pattern(base_pattern, j)
-        i = 0
-        pattern = []
-        while 1:
-            pattern.extend([next(gen) for _ in range(n)])
-            if i != 0 and pattern[:n] == pattern[-n:]:
-                cycle_time.append(i)
-                break
-            i += 1
-    return lcm(cycle_time)
+def next_n(gen, n):
+    for _ in range(n):
+        yield next(gen)
 
 
-def lcm(a):
-    _lcm = a[0]
-    for x in a[1:]:
-        _lcm = (_lcm * x) // math.gcd(_lcm, x)
-    return _lcm
+def last_digit(num):
+    return int(str(num)[-1])
 
+
+def rev_accumulate(arr):
+    s = sum(arr)
+    for x in arr:
+        yield s
+        s -= x
 
 
 if __name__ == '__main__':
-    values_str = "12345678"
+    values_str = "03036732577212944063491565474664" * 10000
+    message_offset = int(values_str[:7])
+
     values = list(map(int, values_str))
 
     base_pattern = (0, 1, 0, -1)
 
-    print(get_cycle_time(base_pattern, len(values_str)))
+    values = values[message_offset:]
+    for _ in range(100):
+        values = list(map(last_digit, rev_accumulate(values)))
 
-    base_pattern_matrix = create_base_pattern_matrix(base_pattern, len(values))
-
-    original_values = list(values)
-    for i in range(4):
-        values = compute_phase(values, base_pattern_matrix)
-    print()
-
-    values_str = "".join(map(str, values))
-    print(values_str)
-    message_offset = values_str[:7]
-    print(values_str[message_offset:message_offset+8])
-
+    print(''.join(map(str, list(values)[:8])))
---
+++ src/rescue.py
@@ -0,0 +1,121 @@
+from src.helpers import read_comma_separated_list, clean_lines_iter
+from src.int_program import IntProgram
+import networkx as nx
+import dwave_networkx as dnx
+import dimod
+
+
+def is_intersection(lines, i, j, n, m):
+    if i < 1 or i > n - 2 or j < 1 or j > m - 2:
+        return False
+    return all(lines[k][l] == "#" for k, l in ((i, j), (i-1, j), (i+1, j), (i, j-1), (i, j+1)))
+
+
+new_direction = {
+    ("U", 0): "L", ("U", 1) : "R",
+    ("D", 0): "R", ("D", 1): "L",
+    ("R", 0): "U", ("R", 1): "D",
+    ("L", 0): "D", ("L", 1): "U",
+}
+
+move_one_step_in_direction = {
+    "R": lambda i, j: (i, j+1),
+    "L": lambda i, j: (i, j-1),
+    "U": lambda i, j: (i-1, j),
+    "D": lambda i, j: (i+1, j)
+}
+
+rotate = {"U": "R", "R": "D", "D": "L", "L": "U"}
+rotate_counter = dict((v, k) for k, v in rotate.items())
+
+
+def can_move(lines, i, j, direction):
+    k, l = move_one_step_in_direction[direction](i, j)
+    return -1 < k < n and -1 < l < m and lines[k][l] == "#"
+
+
+if __name__ == '__main__':
+    memory = read_comma_separated_list("rescue.txt", int)
+    program = IntProgram(memory)
+    program.run()
+    s = ''.join(map(chr, program.output))
+
+    lines = list(clean_lines_iter(s))
+    n = len(lines)
+    m = None
+
+    intersections = []
+    # graph = nx.Graph()
+
+    robot_or_scaffold = "#>^v<"
+
+    robot = None
+    n_scaffolds = 0
+
+    for i, line in enumerate(lines):
+        if m is None:
+            m = len(line)
+        for j, c in enumerate(line):
+            if lines[i][j] in robot_or_scaffold:
+                n_scaffolds += 1
+                if robot_or_scaffold.index(lines[i][j]) > 0:
+                    robot = (i, j)
+                # for k, l in (i-1, j), (i+1, j), (i, j-1), (i, j+1):
+                #     if k < 0 or l < 0:
+                #         continue
+                #     try:
+                #         if lines[k][l] in robot_or_scaffold:
+                #             graph.add_edge((i, j), (k, l))
+                #     except IndexError:
+                #         continue
+
+            if is_intersection(lines, i, j, n, m):
+                intersections.append(i*j)
+
+    print(n_scaffolds)
+    print(s)
+
+    i, j = robot
+    direction = "UDLR"["^v<>".index(lines[i][j])]
+    n_visited = 1
+
+    operations = []
+
+    while n_visited < n_scaffolds:
+        if can_move(lines, i, j, direction):
+            pass
+        elif can_move(lines, i, j, rotate[direction]):
+            direction = rotate[direction]
+            operations.append("ROT")
+        elif can_move(lines, i, j, rotate_counter[direction]):
+            direction = rotate_counter[direction]
+            operations.append("ROT_COUNTER")
+        else:
+            direction = rotate[rotate[direction]]
+            operations.append("ROT")
+            operations.append("ROT")
+
+        operations.append(direction)
+        i, j = move_one_step_in_direction[direction](i, j)
+
+        n_visited += 1
+
+    print(operations)
+
+    # graph = graph.to_undirected()
+    #
+    # paths = dict(nx.all_pairs_shortest_path(graph))
+    #
+    # graph2 = nx.Graph()
+    # edges = []
+    #
+    # for node in graph.nodes:
+    #     for other_node in graph.nodes:
+    #         if other_node == node:
+    #             continue
+    #         edges.append([node, other_node, len(paths[node][other_node])])
+    #
+    # graph2.add_weighted_edges_from(edges)
+    # print(dnx.traveling_salesperson(graph2, dimod.ExactSolver(), start=robot))
+
+
