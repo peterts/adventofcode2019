46722b7d970b7ae902db4440f446aee8f3b7d360
= <petthosan@gmail.com>
2019-12-20 22:11:11 +0100

    Delete old maze.py

--- src/maze.py
+++ src/maze.py
@@ -8,14 +8,32 @@ def build_map(map_lines):
     portals = _find_portals(map_lines, tiles_and_portals)
     passage = _find_passage(map_lines, tiles_and_portals)
 
+    port_vals = [p for positions in portals.values() for p in positions]
+    min_y, max_y = min(port_vals, key=lambda p: p[0])[0], max(port_vals, key=lambda p: p[0])[0]
+    min_x, max_x = min(port_vals, key=lambda p: p[1])[1], max(port_vals, key=lambda p: p[1])[1]
+
     start = portals.pop("AA")[0]
     end = portals.pop("ZZ")[0]
 
+    def _is_inside(_pos):
+        y, x = _pos
+        return min_x < x < max_x and min_y < y < max_y
+
+    negative_transports = []
+    positive_transports = []
+
     for pos1, pos2 in portals.values():
+        if _is_inside(pos1):
+            positive_transports.append((pos1, pos2))
+            negative_transports.append((pos2, pos1))
+        else:
+            positive_transports.append((pos2, pos1))
+            negative_transports.append((pos1, pos2))
+
         passage[pos1].add(pos2)
         passage[pos2].add(pos1)
 
-    return start, end, passage
+    return start, end, passage, negative_transports, positive_transports
 
 
 def _find_portals(map_lines, tiles_and_portals):
@@ -71,11 +89,15 @@ move_one_step_in_direction = {
 opposite_direction = {"R": "L", "L": "R", "U": "D", "D": "U"}
 
 
-def _find_pattern_in_map(map_lines, x):
+def _find_pattern_in_map(map_lines, x, return_first=False):
     found = []
+
     for i, line in enumerate(map_lines):
         for match in re.finditer(x, line):
             found.append((i, match.start()))
+            if return_first:
+                return found
+
     return set(found)
 
 
@@ -95,19 +117,30 @@ def _is_portal(map_lines, pos):
     return _get(map_lines, pos).isupper()
 
 
-def shortest_path(start, end, passage):
+def shortest_path(start, end, passage, negative=(), positive=()):
     visited = set()
-    queue = [(start, 0)]
+    queue = [(start, 0, 0)]
 
     while queue:
-        pos, dist = queue.pop(0)
-        visited.add(pos)
-        if pos == end:
+        pos, dist, state = queue.pop(0)
+        visited.add((pos, state))
+
+        if (pos, state) == (end, 0):
             return dist
+
         for other_pos in passage[pos]:
-            if other_pos in visited:
+            other_state = state
+            if (pos, other_pos) in negative:
+                other_state -= 1
+            elif (pos, other_pos) in positive:
+                other_state += 1
+            if other_state < 0:
                 continue
-            queue.append((other_pos, dist+1))
+
+            if (other_pos, other_state) in visited:
+                continue
+
+            queue.append((other_pos, dist+1, other_state))
 
     return None
 
@@ -117,6 +150,10 @@ if __name__ == '__main__':
         maze_str = "".join(filter(lambda x: x.strip(), f))
 
     maze_str_lines = maze_str.splitlines()
-    start, end, passage = build_map(maze_str_lines)
+    start, end, passage, negative_transports, positive_transports = build_map(maze_str_lines)
     print(shortest_path(start, end, passage))
+    print(shortest_path(start, end, passage, negative_transports, positive_transports))
+
+
+
 
